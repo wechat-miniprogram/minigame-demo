6d3e65ad445505777ce3c6011cdc137abd4d9b5b
v_yoyuliu <v_yoyuliu@tencent.com>
2019-11-25 12:52:09 +0800

    数据上报，埋点

--- miniprogram/js/api/network/request/index.js
+++ miniprogram/js/api/network/request/index.js
@@ -5,6 +5,7 @@ module.exports = function(PIXI, app, obj) {
         let time = Date.now();
         request(res => {
             func(res, time); //绘制信息
+            wx.reportPerformance && wx.reportPerformance(1001, time); // 上报请求总时间的耗时
         });
     });
 };
