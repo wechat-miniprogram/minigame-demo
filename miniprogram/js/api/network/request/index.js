dd7115ebdc41a4da5f78eab37dcad5d057477b08
v_yoyuliu <v_yoyuliu@tencent.com>
2019-11-25 13:24:38 +0800

    修复请求上报总时长错误问题

--- miniprogram/js/api/network/request/index.js
+++ miniprogram/js/api/network/request/index.js
@@ -4,8 +4,9 @@ module.exports = function(PIXI, app, obj) {
     return view(PIXI, app, obj, func => {
         let time = Date.now();
         request(res => {
+            wx.reportPerformance && wx.reportPerformance(1001, Date.now() - time); // 上报请求总时间的耗时
+
             func(res, time); //绘制信息
-            wx.reportPerformance && wx.reportPerformance(1001, time); // 上报请求总时间的耗时
         });
     });
 };
